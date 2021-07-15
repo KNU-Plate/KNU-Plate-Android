89bd355d949490e2f809c344d460b60ea6306035
Glacier Han <hbh6449@naver.com>
2021-07-15 14:28:13 +0900

    -

---
+++ app/src/main/java/com/example/knuplate/MyTabActivity.java
@@ -0,0 +1,14 @@
+package com.example.knuplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyTabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tab);
+    }
+}
