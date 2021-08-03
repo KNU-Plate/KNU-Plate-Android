6356ea1bdbe03ab9d3a58659528009e3446c5fde
Glacier Han <hbh6449@naver.com>
2021-08-04 01:32:08 +0900

    210804

--- app/src/main/java/com/example/knuplate/MainActivity.java
+++ app/src/main/java/com/example/knuplate/MainActivity.java
@@ -4,33 +4,68 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.example.knuplate.Adapter.RestAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
+    private RestAdapter adapter;
+    private LinearLayout bt_northgate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
+        recyclerView = findViewById(R.id.restList);
+        gridLayoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(gridLayoutManager);
 
-        Button testbut = findViewById(R.id.testbutton);
-        testbut.setOnClickListener(new View.OnClickListener() {
+        //어댑터 세팅
+        adapter = new RestAdapter();
+        recyclerView.setAdapter(adapter);
+
+        bt_northgate = findViewById(R.id.bt_northgate);
+        bt_northgate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),DetailActivity.class);
+                Intent intent = new Intent(MainActivity.this, RestListActivity.class);
                 startActivity(intent);
             }
         });
 
+
+        //BottomNav
+        BottomNavigationView bottom_menu = (BottomNavigationView) findViewById(R.id.bottom_menu);
+        bottom_menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab1:
+                        Intent intent1 = new Intent(getApplicationContext(), RestListActivity.class);
+                        startActivity(intent1);
+                        return true;
+                    case R.id.tab2:
+                        return true;
+                    case R.id.tab3:
+                        Intent intent3 = new Intent(getApplicationContext(), MyTabActivity.class);
+                        startActivity(intent3);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
