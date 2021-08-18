466b32e73ab906f72dbc8e88faa30e4500806982
Glacier Han <hbh6449@naver.com>
2021-08-18 23:52:45 +0900

    Location Permission Code + App Icon

--- app/src/main/java/com/example/knuplate/DetailActivity.java
+++ app/src/main/java/com/example/knuplate/DetailActivity.java
@@ -5,16 +5,23 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -184,6 +191,19 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
+        //권한
+        setPermissionCheckListener(new PermissionCheckListener() {
+            @Override
+            public void permissionCheckFinish() {
+                Log.e("log", "permission_complete");
+            }
+        });
+
+        if (permissionCheck(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})) {
+            return;
+        }
+        permissionCheckListener.permissionCheckFinish();
+
     }
 
     //앨범 이미지 가져오기
@@ -308,6 +328,78 @@ public class DetailActivity extends AppCompatActivity {
             Log.e(TAG, cbTAG + "레트로핏 콜백 요청 실패(2) "+ t);
         }
     };
+
+
+    //권한 허가 코드
+    public interface PermissionCheckListener {
+        void permissionCheckFinish();
+    }
+
+    public PermissionCheckListener permissionCheckListener;
+
+    public void setPermissionCheckListener(PermissionCheckListener permissionCheckListener) {
+        this.permissionCheckListener = permissionCheckListener;
+    }
+
+    public boolean permissionCheck(String[] strings) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (int i = 0; i < strings.length; i++) {
+                if (ContextCompat.checkSelfPermission(this, strings[i]) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(this, strings, 1);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            //퍼미션 체크.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                for (int i = 0; i < grantResults.length; i++) {
+                    if (grantResults[i] == 0) {
+                        if (grantResults.length == (i + 1)) {
+                            permissionCheckListener.permissionCheckFinish();
+                        }
+                    } else {
+                        // 거부한 이력이 있으면 true를 반환한다.
+                        if (shouldShowRequestPermissionRationale(permissions[i])) {
+                            ActivityCompat.requestPermissions(this, permissions, 1);
+                            break;
+                        } else {
+                            new AlertDialog.Builder(this)
+                                    .setTitle("다시보지않기 클릭.")
+                                    .setMessage(permissions[i] + " 권한이 거부되었습니다 설정에서 직접 권한을 허용 해주세요.")
+                                    .setNeutralButton("설정", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                            intent.setData(Uri.parse("package:" + getPackageName()));
+                                            startActivity(intent);
+                                            Toast.makeText(getApplicationContext(), "권한 설정 후 다시 실행 해주세요.", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    })
+                                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Toast.makeText(getApplicationContext(), "권한 설정을 하셔야 앱을 사용할 수 있습니다.", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    })
+                                    .setCancelable(false)
+                                    .create().show();
+                        }// shouldShowRequestPermissionRationale /else
+                    } // 권한 거절
+                } // for end
+            }//Build.VERSION.SDK_INT  end
+        }//requestCode  end
+    }
 
 } //end of Activity
 
--- app/src/main/java/com/example/knuplate/Fragment/Fragment_Location.java
+++ app/src/main/java/com/example/knuplate/Fragment/Fragment_Location.java
@@ -1,6 +1,6 @@
 package com.example.knuplate.Fragment;
 
-import android.os.Bundle;
+import  android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
